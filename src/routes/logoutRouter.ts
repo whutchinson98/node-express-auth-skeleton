4d774a8cda22c9653efbb9ef4fa38167b276d538
whutchinson98 <willhutchinson2.0@gmail.com>
2021-04-09 21:36:33 -0400

    testing

--- src/routes/logoutRouter.ts
+++ src/routes/logoutRouter.ts
@@ -3,9 +3,9 @@ import {Router} from 'express';
 /* eslint-disable-next-line new-cap */
 const router = Router();
 
-const {login} = require('../controllers/authorizationController');
+const {logout} = require('../controllers/authorizationController');
 
 // ROUTES
-router.post('/login', login);
+router.get('/logout', logout);
 
 module.exports = router;
