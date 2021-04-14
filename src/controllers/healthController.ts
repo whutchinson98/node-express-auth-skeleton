8b4b6bbe9b80d26889ca9b3c1835ab729e03280e
whutchinson98 <willhutchinson2.0@gmail.com>
2021-04-14 10:45:20 -0400

    lint fix

--- src/controllers/healthController.ts
+++ src/controllers/healthController.ts
@@ -1,33 +1,36 @@
-import {Response,Request} from 'express';
+import {Response, Request} from 'express';
 import mongoose from 'mongoose';
 
-const mongooseConnectionStates = ['DISCONNECTED', 'CONNECTED', 'CONNECTING','DISCONNECTING'];
+const mongooseConnectionStates = ['DISCONNECTED',
+  'CONNECTED',
+  'CONNECTING',
+  'DISCONNECTING',
+];
 
 const connections = async (req: Request, res: Response) => {
-  try{
+  try {
     const state = mongooseConnectionStates[mongoose.connection.readyState];
     return res.status(200).json({
       error: false,
-      mongoose: state
+      mongoose: state,
     });
-
-  }catch(err){
+  } catch (err) {
     console.log(err);
     return res.status(500).json({
-      error:true,
-      message: 'Error occured fetching connection state.'
+      error: true,
+      message: 'Error occured fetching connection state.',
     });
   }
 };
 
 const misc = async (req: Request, res: Response) => {
   return res.status(200).json({
-    error:false,
-    message: 'Healthy'
+    error: false,
+    message: 'Healthy',
   });
 };
 
 module.exports = {
   connections,
-  misc
+  misc,
 };
